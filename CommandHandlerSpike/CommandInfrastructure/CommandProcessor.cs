4dbb2cc29bda52d1042f341f2504cf1c31749575
Dan Ryan <mail.dan.ryan@googlemail.com>
2015-10-01 12:43:03 +0100

    Remove old code.

--- CommandHandlerSpike/CommandInfrastructure/CommandProcessor.cs
+++ CommandHandlerSpike/CommandInfrastructure/CommandProcessor.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.Linq.Expressions;
-using System.Reflection;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace CommandHandlerSpike.CommandInfrastructure
 {
@@ -21,41 +17,9 @@ namespace CommandHandlerSpike.CommandInfrastructure
 
         public async Task Execute<TCommand>(TCommand command) where TCommand : class, ICommand
         {
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
             var handlerType = _commandRegistry.GetHandlerForCommand<TCommand>();
             var handler = (ICommandHandler<TCommand>)_commandHandlerFactory.Get(handlerType);
             await handler.Execute(command);
-
-            sw.Stop();
-            Console.WriteLine(sw.ElapsedMilliseconds);
-        }
-
-        
-        [DebuggerStepThrough]
-        public async Task<TResult> ExecuteQuery<TResult>(IQuery<TResult> query)
-        {
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            MethodInfo getHandlerForCommandMethod = GetMethod<ICommandRegistry>(x => x.GetHandlerForCommand<ICommand>()).MakeGenericMethod(query.GetType());
-            var handlerType = (Type)getHandlerForCommandMethod.Invoke(_commandRegistry, null);
-            dynamic handler = _commandHandlerFactory.Get(handlerType);
-
-            
-            
-            await handler.Execute((dynamic)query);
-
-            sw.Stop();
-            Console.WriteLine(sw.ElapsedMilliseconds);
-
-            return query.Result;
-        }
-
-        public static MethodInfo GetMethod<T>(Expression<Action<T>> expr)
-        {
-            return((MethodCallExpression)expr.Body)
-                .Method
-                .GetGenericMethodDefinition();
         }
     }
 }
